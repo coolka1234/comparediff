17c5784039813a9915934341bb0aadc9506e2d78
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-08-06 18:03:34 +0200

    statring on buffer reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("foo.txt")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
+
+
